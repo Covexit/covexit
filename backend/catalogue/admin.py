7e64970b64979e5384cd1a525e3cc7aa49007180
Ed Rivas <ed@unplug.studio>
2020-03-27 16:42:02 -0600

    Catalogue: improve product admin

--- backend/catalogue/admin.py
+++ backend/catalogue/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from .models import ProductCategory, Product
 
 
-admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "updated", "company", "price", "stock", "category"]
+    list_editable = ["category", "stock"]
+    list_filter = ["company", "category"]
+    date_hierarchy = "updated"
