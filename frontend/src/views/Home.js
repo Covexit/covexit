5a047a330bc5606c9495f3eb16905f99e907c619
kvzivn <hello@kevinivan.com>
2020-03-28 18:02:57 +0800

    break out styles

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -3,12 +3,12 @@ import Button from '../components/Button/Button';
 
 const Home = () => (
   <div className="Home">
-    <div className="Home-hero">
-      <h1 className="Home-heading">Two line description<br />of the idea and service.</h1>
-      <p className="Home-text">Just a tiny bit more explanation, since two lines probably won’t tell enough.</p>
+    <div className="Intro">
+      <h1>Two line description<br />of the idea and service.</h1>
+      <p>Just a tiny bit more explanation, since two lines probably won’t tell enough.</p>
     </div>
 
-    <div className="Home-btns">
+    <div className="Btn-group">
       <Button to="/map" label="Search for shops nearby!" />
       <Button to="/company" label="Register your business" secondary />
     </div>
