d844187855bdf86d14d3e114b4f81c709021ac43
kvzivn <hello@kevinivan.com>
2020-03-28 18:02:37 +0800

    refactor

--- frontend/src/app/Routes.js
+++ frontend/src/app/Routes.js
@@ -4,7 +4,6 @@ import NoMatch from "views/NoMatch"
 import Home from "views/Home"
 import Map from "components/Map/Map"
 
-
 // Placeholders for now
 // use the route for the HomeView as reference for all others
 // eg: <Route path="/my-view" component={MyView} />
@@ -21,9 +20,7 @@ function Routes() {
         <h2>Insert company backend view (falls back to login) if user has a
           company show edit, otherwise create view</h2>
       </Route>
-      <Route path="/map">
-        <Map />
-      </Route>
+      <Route path="/map" component={Map} />
       <Route exact path="/" component={Home} />
       <Route component={NoMatch} />
     </Switch>
