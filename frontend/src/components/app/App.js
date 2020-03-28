9b27ce4dbb640c87fbbc381a9860efd83c1aeef7
Philipp Veller <vellerphilipp@gmail.com>
2020-03-28 01:04:32 +0100

    add a loader component for user feedback when loading stuff with the API

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from 'assets/logo.svg';
 import './App.scss';
+import Loader from "../loader/Loader";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is covexit! Woo!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <Loader/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
