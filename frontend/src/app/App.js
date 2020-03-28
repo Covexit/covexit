cf155e39d46dfcb5979545661fea47c09dbec830
Philipp Veller <vellerphilipp@gmail.com>
2020-03-28 03:25:03 +0100

    move app into its own directory

---
+++ frontend/src/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Routes from "./Routes";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is covexit! Woo!</h1>
+      </header>
+      <Routes/>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.scss';
-import App from './components/app/App';
+import App from './app/App';
 import * as serviceWorker from 'serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
