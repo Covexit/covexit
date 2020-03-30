c65351d1d0ecf3635c062f096922fb9ad2726084
kvzivn <hello@kevinivan.com>
2020-03-30 18:32:21 +0800

    add business images

---
+++ frontend/src/shared/businessImages.js
@@ -0,0 +1,37 @@
+import other from '../assets/business_other.jpg'
+import clothes from '../assets/business_clothes.jpg'
+import electronics from '../assets/business_electronics.jpg'
+import jewellry from '../assets/business_jewellry.jpg'
+import books from '../assets/business_books.jpg'
+import garden from '../assets/business_garden.jpg'
+import drugstore from '../assets/business_drugstore.jpg'
+import toys from '../assets/business_toys.jpg'
+import coffee from '../assets/business_coffee.jpg'
+import living from '../assets/business_living.jpg'
+
+import bakery1 from '../assets/bakery1.jpg'
+import bakery2 from '../assets/bakery2.jpg'
+import bakery3 from '../assets/bakery3.jpg'
+import bakery4 from '../assets/bakery4.jpg'
+import bakery5 from '../assets/bakery5.jpg'
+
+export const businessImages = [
+  other,
+  clothes,
+  electronics,
+  jewellry,
+  books,
+  garden,
+  drugstore,
+  toys,
+  coffee,
+  living
+]
+
+export const bakeryImages = [
+  bakery1,
+  bakery2,
+  bakery3,
+  bakery4,
+  bakery5
+]
