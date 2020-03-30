dce522c6cd14900c3de63f2c90969810403a82f5
Philipp Veller <vellerphilipp@gmail.com>
2020-03-30 22:11:16 +0200

    render either Link or button based on props for <Button />

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Button = ({ to, label, secondary, type }) => (
-  <Link to={to} className={`Btn ${secondary && `Btn--secondary`} ${type && `Btn--${type}`}`}>
-    {label}
-  </Link>
-)
+const Button = ({ to, onClick, label, secondary, type }) => {
+  const classes = `Btn ${secondary && `Btn--secondary`} ${type && `Btn--${type}`}`;
+
+  if (to)
+    return (
+      <Link to={to} className={classes} onClick={onClick}>
+        {label}
+      </Link>
+    );
+
+  return <button onClick={onClick} className={classes}>{label}</button>;
+};
 
 export default Button
