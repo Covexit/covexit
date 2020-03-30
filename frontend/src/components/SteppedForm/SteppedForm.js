7cd5ffe0b37389b709b5e34c9bf589b8c9338950
Philipp Veller <vellerphilipp@gmail.com>
2020-03-30 12:23:39 +0200

    use object destructuring on SteppedForm.js

--- frontend/src/components/SteppedForm/SteppedForm.js
+++ frontend/src/components/SteppedForm/SteppedForm.js
@@ -4,20 +4,20 @@ import React from "react";
 import './SteppedForm.scss'
 import Stepper from "../Stepper/Stepper";
 
-const SteppedForm = (props) => {
-  const inputs = props.inputs &&
-    props.inputs.map((e) =>
-      <TextInput {...e} onChange={props.onChange} key={e.name}/>);
+const SteppedForm = ({inputs, onChange, stepperProps, head, body, footer}) => {
+  const renderedInputs = inputs &&
+    inputs.map((e) =>
+      <TextInput {...e} onChange={onChange} key={e.name}/>);
 
-  const stepper = props.stepperProps && <Stepper {...props.stepperProps}/>;
+  const stepper = stepperProps && <Stepper {...stepperProps}/>;
 
   return (
     <div className="SteppedForm">
-      <div className="SteppedForm-head Intro">{props.head}</div>
+      <div className="SteppedForm-head Intro">{head}</div>
       <div className="SteppedForm-body">
-        {inputs || props.body}
+        {renderedInputs || body}
       </div>
-      <div className="SteppedForm-footer">{stepper}{props.footer}</div>
+      <div className="SteppedForm-footer">{stepper}{footer}</div>
     </div>
   );
 };
