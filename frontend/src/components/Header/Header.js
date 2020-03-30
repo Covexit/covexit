9865b820ed265773aa252a6f4e81461f92f2b920
Philipp Veller <vellerphilipp@gmail.com>
2020-03-30 12:15:29 +0200

    add header component with logo

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.scss';
+
+import logo from 'assets/logo.svg';
+import share from 'assets/share.svg';
+import Menu from "../Menu/Menu";
+import Share from "../Share/Share";
+
+function Header() {
+  return (
+    <div className="Header">
+      <Menu/>
+      <img src={logo} alt="Covexit Logo" className="Header-logo"/>
+      <Share/>
+    </div>
+  );
+}
+
+export default Header;
