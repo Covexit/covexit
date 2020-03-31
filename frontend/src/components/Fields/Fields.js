50e68589e802c02261ee9b920e87ab34aad99798
Philipp Veller <vellerphilipp@gmail.com>
2020-03-31 02:00:32 +0200

    use props destructuring and add new form fields

--- frontend/src/components/Fields/Fields.js
+++ frontend/src/components/Fields/Fields.js
@@ -1,19 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Fields.scss';
+import Button from "../Button/Button";
 
 
-const TextInput = (props) => {
+const TextInput = ({ onKeyDown, placeholder, type, onChange, onFocus, onBlur, name,
+                    value }) => {
   return (
-    <div className="TextInput" onKeyDown={props.onKeyDown}>
+    <div className="TextInput" onKeyDown={onKeyDown}>
       <label>
-        <span className="TextInput-label-text">{props.placeholder}</span>
-        <input type={props.type || 'text'} className="TextInput-field" onChange={props.onChange}
-               onFocus={props.onFocus} onBlur={props.onBlur} name={props.name}
-               value={props.value} placeholder={props.placeholder} />
-       </label>
+        <span className="TextInput-label-text">{placeholder}</span>
+        <input type={type || 'text'} className="TextInput-field" onChange={onChange}
+               onFocus={onFocus} onBlur={onBlur} name={name}
+               value={value} placeholder={placeholder}/>
+      </label>
     </div>
   );
 };
 
-export default { TextInput }
+const TextArea = ({ onKeyDown, placeholder, rows, onChange, onFocus, onBlur, name,
+                    value }) => {
+  return (
+    <div className="TextArea" onKeyDown={onKeyDown}>
+      <label>
+        <span className="TextInput-label-text">{placeholder}</span>
+        <textarea className="TextInput-field" onChange={onChange}
+                  onFocus={onFocus} onBlur={onBlur} name={name} rows={rows || 3}
+                  value={value} placeholder={placeholder}/>
+      </label>
+    </div>
+  );
+};
+
+const FileUpload = ({ helpText, label, onChange, name, value }) => {
+  const [img, setImg] = useState();
+  const file = value && value[0];
+
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = reader => setImg(reader.target.result);
+    reader.readAsDataURL(file);
+  }
+
+  return (
+    <div className="FileUpload">
+      {img && file &&
+      <div className="FileUpload-preview TextInput-field">
+        <img src={img} alt={file.name} className="FileUpload-img" />
+        <span>{file.name}</span>
+      </div>}
+      <label>
+        <Button secondary label={label} span className="test" />
+        <span className="TextInput-helpText">{helpText}</span>
+        <input type="file" className="FileUpload-field" onChange={onChange}
+               name={name}/>
+      </label>
+    </div>
+  );
+};
+
+export default { TextInput, TextArea, FileUpload }
