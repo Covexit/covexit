89da4df0c3465babed6224f463bbbff4488e52a9
Philipp Veller <vellerphilipp@gmail.com>
2020-04-01 22:16:12 +0200

    add desktop header style, link logo

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,12 +4,13 @@ import './Header.scss';
 import logo from 'assets/logo.svg';
 import Menu from "../Menu/Menu";
 import Share from "../Share/Share";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="Header">
       <Menu/>
-      <img src={logo} alt="Covexit Logo" className="Header-logo"/>
+      <Link to="/"><img src={logo} alt="Covexit Logo" className="Header-logo"/></Link>
       <Share/>
     </div>
   );
