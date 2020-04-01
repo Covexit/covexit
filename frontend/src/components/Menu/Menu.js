8ed091bc8917e3656cc332e551832d46e2331363
Philipp Veller <vellerphilipp@gmail.com>
2020-04-01 21:23:16 +0200

    add mechanism for external links in menu and add explanatory link to menu

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -10,6 +10,7 @@ function Menu() {
     { to: '/map', label: 'Map test' },
     { to: '/order/0', label: 'Order test' },
     { to: '/stores/1', label: 'Store test' },
+    { to: 'https://covexit.webflow.io/', label: 'How it works', external: true },
     { to: '/imprint', label: 'Imprint', meta: true },
     { to: '/privacy', label: 'Privacy', meta: true },
   ];
@@ -24,7 +25,8 @@ function Menu() {
         <ul className="Menu-list">
           {links.map(e =>
             <li className="Menu-list-item" key={e.label}>
-              <NavLink to={e.to} onClick={() => setMenuOpen(false)} className={`Menu-link ${e.meta && 'Menu-link--meta'}`}>{e.label}</NavLink>
+              {(e.external && <a href={e.to} className="Menu-link">{e.label}</a>) ||
+              <NavLink to={e.to} onClick={() => setMenuOpen(false)} className={`Menu-link ${e.meta && 'Menu-link--meta'}`}>{e.label}</NavLink>}
             </li>)}
         </ul>
         <div className="Menu-footer">© 2020 Covexit</div>
