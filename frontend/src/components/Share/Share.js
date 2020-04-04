ed7745f32dedf95b2d7988c17079d0647a0fe742
blueag8 <41134758+blueag8@users.noreply.github.com>
2020-04-04 17:31:25 +0100

    first commit
    Added Modal and updated Share

--- frontend/src/components/Share/Share.js
+++ frontend/src/components/Share/Share.js
@@ -1,21 +1,75 @@
 import React from 'react';
 import './Share.scss';
+import Modal from "../Modal/Modal"
 import share from "../../assets/share.svg";
 
+class Share extends React.Component {
+  state = {
+    show: false
+  };
+  showModal = e => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+    constructor(props) {
+       super(props)
 
-function Share() {
-  return (
-    <div className="Share">
-      <button className="Share-toggle">
-        <img src={share} alt="Share Icon" className="Share-icon"/>
+        this.state = {
+        copySuccess: false
+    }
+  }
+ copyCodeToClipboard = () => {
+    const urlRef= window.location.href;
+    this.urlRef.select();
 
-        Share
-      </button>
-      <div className="Share-body">
+    document.execCommand("copy")
+    this.setState({copySuccess: true})
 
-      </div>
-    </div>
-  );
+  }
+
+  handleCopy (e) {
+    e.preventDefault();
+    e.clipboardData.setData("url","www.covexit.de");
+  }
+  render() {
+    return (
+      <div className="Share">
+        <button
+          class="toggle-button"
+          id="centered-toggle-button"
+          onClick={e => {
+            this.showModal(e);
+          }}
+        ><img src={share} alt="Share Icon" className="Share-icon" />
+          {" "}
+         Share{" "}
+        </button>
+
+        <Modal onClose={this.showModal} show={this.state.show}>
+          <p class="message">Share Covexit and help others with this crisis.</p>
+         {
+          this.state.copySuccess ?
+            <div id="success">
+           ∞ copied!
+            </div> : null
+
+           }
+          <input id="copy"
+            disabled
+            ref={(urlRef) => this.urlRef = urlRef}
+            placeholder="www.covexit.de"
+          />
+
+
+   <button class="Btn" onClick={() => this.copyCodeToClipboard()}>
+            Copy Link
+          </button>
+
+        </Modal>
+</div>
+    );
+  }
 }
 
 export default Share;
