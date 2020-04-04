23d78d80cb9f796ba0bfa09abdff4710b7152389
Philipp Veller <vellerphilipp@gmail.com>
2020-04-04 13:39:26 +0200

    remove companies app

--- backend/companies/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Company
-
-
-class CompanySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Company
-        fields = ['id', 'name', 'mail', 'street', 'city', 'phone', 'zip',
-                  'latitude', 'longitude', 'owner', 'industry', ]
--- backend/covexit/urls.py
+++ backend/covexit/urls.py
@@ -17,13 +17,8 @@ from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
-from companies.views import CompanyViewSet
-
-router = DefaultRouter()
-router.register(r'company', CompanyViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('oscarapi.urls')),
-    path('api/v1/', include(router.urls)),
 ]
