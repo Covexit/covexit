010c4d81be311d08adba6965e830ee7a7511904f
Philipp Veller <vellerphilipp@gmail.com>
2020-04-04 13:42:18 +0200

    fork oscar partner

--- backend/partner/models.py
+++ backend/partner/models.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from oscar.apps.partner.abstract_models import AbstractPartner
 
 
-class Company(models.Model):
+class Partner(AbstractPartner):
     """
     Model to store company information
     """
@@ -22,8 +21,6 @@ class Company(models.Model):
     COFFEE = 'CF', _('Tea and Coffee')
     LIVING = 'LV', _('Household and Living')
 
-    name_validator = UnicodeUsernameValidator()
-
     created = models.DateTimeField(_("Created"), auto_now_add=True)
     updated = models.DateTimeField(_("Last updated"), auto_now=True)
 
@@ -33,23 +30,11 @@ class Company(models.Model):
         unique=True,
         help_text=_('65 characters or fewer. '
                     'Letters, digits and @/./+/-/_ only.'),
-        validators=[name_validator],
         error_messages={
             'unique': _('A company with that name already exists.'),
         },
     )
-    mail = models.EmailField(_('e-mail address'), max_length=80)
-    website = models.CharField(_('website'), max_length=80, blank=True,
-                               null=True)
-    street = models.CharField(_('street address'), max_length=60)
-    city = models.CharField(_('city'), max_length=45)
-    phone = models.CharField(_('phone'), max_length=45)
-    zip = models.CharField(_('ZIP Code'), max_length=10)
-    latitude = models.DecimalField(_('latitude'), max_digits=9,
-                                   decimal_places=6)
-    longitude = models.DecimalField(_('longitude'), max_digits=9,
-                                    decimal_places=6)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
     industry = models.CharField(
         max_length=2,
         choices=[CLOTHES, ELECTRONICS, JEWELLERY, BOOKS, GARDEN, DRUGSTORE,
@@ -59,3 +44,6 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+
+
+from oscar.apps.partner.models import *  # noqa isort:skip
