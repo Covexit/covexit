14f4b45672250e22df588234bffdbaba6e3b0e30
Philipp Veller <vellerphilipp@gmail.com>
2020-04-05 14:22:01 +0200

    add migrations for product class data and initial product attribute, remove obsolete industry field

--- backend/covexit/partner/models.py
+++ backend/covexit/partner/models.py
@@ -12,16 +12,6 @@ class Partner(AbstractPartner):
         verbose_name = _('Company')
         verbose_name_plural = _('Companies')
 
-    CLOTHES = 'CL', _('Clothes and shoes')
-    ELECTRONICS = 'EC', _('Electronics')
-    JEWELLERY = 'JW', _('Accessoires and Jewellery')
-    BOOKS = 'BK', _('Books and Office')
-    GARDEN = 'GD', _('Flowers, Garden and Animals')
-    DRUGSTORE = 'DS', _('Drugstore and Beauy')
-    TOYS = 'TS', _('Toys and Gifts')
-    COFFEE = 'CF', _('Tea and Coffee')
-    LIVING = 'LV', _('Household and Living')
-
     created = models.DateTimeField(_("Created"), auto_now_add=True)
     updated = models.DateTimeField(_("Last updated"), auto_now=True)
 
@@ -34,13 +24,6 @@ class Partner(AbstractPartner):
         error_messages={
             'unique': _('A company with that name already exists.'),
         },
-    )
-
-    industry = models.CharField(
-        max_length=2,
-        choices=[CLOTHES, ELECTRONICS, JEWELLERY, BOOKS, GARDEN, DRUGSTORE,
-                 TOYS, COFFEE, LIVING],
-        default='CL',
     )
 
     def __str__(self):
