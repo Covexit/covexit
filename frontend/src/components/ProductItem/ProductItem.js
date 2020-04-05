185da97049bbe0c2ea5b8623e4103e9fb2888dae
kvzivn <hello@kevinivan.com>
2020-04-05 14:35:24 +0800

    refactor store and product list

--- frontend/src/components/ProductItem/ProductItem.js
+++ frontend/src/components/ProductItem/ProductItem.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 import roundedPlusIcon from "../../assets/rounded_plus.svg";
-import penIcon from "../../assets/pen.svg";
 
-const ProductItem = ({ type, price, image }) => {
-  const productIcons = {
-    add: roundedPlusIcon,
-    edit: penIcon,
-    remove: penIcon,
-  }
+const ProductItem = ({ name, description, price, image }) => {
+  // const productIcons = {
+  //   add: roundedPlusIcon,
+  //   edit: penIcon,
+  //   remove: penIcon,
+  // }
 
   return (
     <div className="Product-item">
       <img className="Product-img" src={image} alt="product" />
       <div className="Product-content">
         <div className="Product-review">
-          <h4>Very great bread indeed</h4>
-          <p>mix of some flourish stuff and water, plus some salty crystals looking like salt</p>
+          <h4>{name}</h4>
+          <p>{description}</p>
           <h4 className="variant-price">{price}€</h4>
         </div>
-        <img className="medium-icon" src={productIcons[type]} alt={`${type} product`} />
+
+        <img className="medium-icon" src={roundedPlusIcon} alt="add product" />
       </div>
     </div>
   );
