6b35888018dceb039848ab903ccfc220f58a4b2c
blueag8 <41134758+blueag8@users.noreply.github.com>
2020-04-07 18:15:24 +0100

    update Modal and Share
    Removed seperate Modal file and updated Share.js and Share.scss. Modal can now be closed if clicked outside of it.

--- frontend/src/components/Modal/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Share from "./Share";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Share />, rootElement);
