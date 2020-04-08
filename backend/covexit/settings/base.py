15e996695a0a581d4c8626b906806cff5d9d4dc7
Philipp Veller <vellerphilipp@gmail.com>
2020-04-08 17:42:10 +0200

    update django static & media paths

--- backend/covexit/settings/base.py
+++ backend/covexit/settings/base.py
@@ -130,6 +130,9 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 """
 Oscar Setting
