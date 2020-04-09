4884cca083801e263a0347597ff6c70f2b5b20a2
Philipp Veller <vellerphilipp@gmail.com>
2020-04-09 19:57:10 +0200

    move company page route nested inside Stores Route

--- frontend/src/app/Routes.js
+++ frontend/src/app/Routes.js
@@ -3,20 +3,15 @@ import React from "react"
 import NoMatch from "views/NoMatch"
 import Home from "views/Home"
 import PhotoSelect from "views/PhotoSelect"
-import CompanyPage from "views/CompanyPage"
 import OrderForm from "../views/OrderForm";
 import Onboarding from "../views/Onboarding";
 import NewStore from "../views/NewStore";
 import Stores from '../views/Stores';
 
-// Placeholders for now
-// use the route for the HomeView as reference for all others
-// eg: <Route path="/my-view" component={MyView} />
 function Routes() {
   return (
     <Switch>
       <Route path="/stores/new" component={NewStore} />
-      <Route exact path="/stores/:id/company" component={CompanyPage} />
       <Route excat path="/stores/:id/onboarding/:step" component={Onboarding} />
       <Route path="/stores" component={Stores} />
       <Route path="/company/photo-select" component={PhotoSelect} />
