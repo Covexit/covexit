a8abf3003ea6ed7c140ad30f18d61550bf3baf86
Philipp Veller <vellerphilipp@gmail.com>
2020-04-09 19:50:25 +0200

    cleaning up routes & menu

--- frontend/src/app/Routes.js
+++ frontend/src/app/Routes.js
@@ -4,7 +4,6 @@ import NoMatch from "views/NoMatch"
 import Home from "views/Home"
 import PhotoSelect from "views/PhotoSelect"
 import CompanyPage from "views/CompanyPage"
-import Map from "components/Map/Map"
 import OrderForm from "../views/OrderForm";
 import Onboarding from "../views/Onboarding";
 import NewStore from "../views/NewStore";
@@ -16,18 +15,12 @@ import Stores from '../views/Stores';
 function Routes() {
   return (
     <Switch>
-
       <Route path="/stores/new" component={NewStore} />
       <Route exact path="/stores/:id/company" component={CompanyPage} />
       <Route excat path="/stores/:id/onboarding/:step" component={Onboarding} />
       <Route path="/stores" component={Stores} />
-      <Route exact path="/company">
-        <h2>Insert company backend view (falls back to login) if user has a
-          company show edit, otherwise create view</h2>
-      </Route>
       <Route path="/company/photo-select" component={PhotoSelect} />
       <Route path="/order/:step" component={OrderForm} />
-      <Route path="/map" component={Map} />
       <Route exact path="/" component={Home} />
       <Route component={NoMatch} />
     </Switch>
