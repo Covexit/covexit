00b0c7910cee80c29cf421dbf11b844f8a255d7f
Philipp Veller <vellerphilipp@gmail.com>
2020-04-13 13:25:51 +0200

    fix verify link creation

--- backend/covexit/account/models.py
+++ backend/covexit/account/models.py
@@ -22,8 +22,7 @@ VERIFICATION_URL = '/verify/'
 
 
 def create_verification_link(user):
-    return (Site.objects.get_current().domain + VERIFICATION_URL +
-            str(user.pk) + '/' + user.verification_key)
+    return (Site.objects.get_current().domain + VERIFICATION_URL + str(user.pk) + '/' + user.verification_key)
 
 
 def create_verification_key():
