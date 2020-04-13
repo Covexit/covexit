6b0eb9913f8f758fdc4f2eaa2cf534000c96fea7
blueag8 <41134758+blueag8@users.noreply.github.com>
2020-04-13 10:01:05 +0100

    clickHandler readded
    Modal closes if clicked outside!

--- frontend/src/components/Share/Share.js
+++ frontend/src/components/Share/Share.js
@@ -3,18 +3,27 @@ import "./Share.scss";
 import share from "../../assets/share.svg";
 import Button from "../Button/Button";
 
-
 function Share() {
-
   const [modalShow, setModalShow] = useState(false);
   const [copySuccess, setCopySuccess] = useState(false);
-  const message = "Share Covexit with others and help during this crisis.";
-  const inputRef = useRef();
-  const urlRef = window.location.href;
+  const message = "Share Covexit with others and help during this crisis!";
+  const urlRef = useRef();
   const url = window.location.href;
+  const modalRef = useRef();
+  const handleClick = () => {
+    setModalShow(!modalShow);
+  };
+  window.addEventListener("click", function (e) {
+    if (e.target === modalRef.current) {
+      setModalShow(false);
+    } else {
+      return;
+    }
+  });
 
   function copyLink() {
-    inputRef.current.select();
+    urlRef.current.select();
+    document.execCommand("copy");
     setCopySuccess(" ∞ copied! ");
     setTimeout(() => {
       setModalShow(false);
@@ -24,21 +33,27 @@ function Share() {
 
   return (
     <div className="Share">
-      <button className="Share-toggle" onClick={() => setModalShow(!modalShow)}>
+      <button className="Share-toggle" onClick={handleClick}>
         <img src={share} alt="Share Icon" className="Share-icon" />
         Share
       </button>
-      <div className={`Modal Modal--${modalShow ? "opened" : "closed"}`}>
+
+      <div
+        className={`Modal Modal--${modalShow ? "opened" : "closed"}`}
+        ref={modalRef}
+      >
         <div className="Modal-body">
           <div className="Share-message">{message}</div>
-          <input type='value' className="TextInput-field"
+          <input
+            type="value"
+            className="TextInput-field"
             defaultValue={url}
             ref={urlRef}
           />
           <div className="Alert">{copySuccess}</div>
           <div className="Btn-group">
             <Button label="Copy Link" onClick={() => copyLink()} />
-            <Button type="dismiss" label="Skip" onClick={() => setModalShow(false)} />
+            <Button type="dismiss" label="Skip" onClick={handleClick} />
           </div>
         </div>
       </div>
@@ -47,4 +62,3 @@ function Share() {
 }
 
 export default Share;
-
