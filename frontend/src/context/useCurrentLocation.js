69b49c4053d0f7d590e61c0546b0e7464fb0d92d
Akolade <adesanmiakoladedotun@gmail.com>
2020-04-14 13:57:52 +0100

    feat: implement user location query

---
+++ frontend/src/context/useCurrentLocation.js
@@ -0,0 +1,91 @@
+import { useEffect, useReducer } from 'react'
+import constate from 'constate'
+import logger from './Logger'
+import useLocalStorage from '../shared/useLocalStorage'
+
+
+const initialState = {
+  selectedLocation: {},
+  coordinates: []
+};
+
+
+const reducer = (originalState, action) => {
+  let state = Object.assign({}, originalState);
+  switch (action.type) {
+    case 'SET_SELECTED_COORDINATES':
+      return {
+        ...state,
+        coordinates: action.payload.coordinates,
+        selectedLocation: {},
+      }
+
+    case 'SET_SELECTED_LOCATION': 
+      const { suggestion, coordinates } = action.payload;
+      return {
+        ...state,
+        selectedLocation: suggestion,
+        coordinates,
+      };
+
+    default: {
+      return state;
+    }
+  }
+
+};
+
+const loggerReducer = logger(reducer);
+
+const useCurrentLocation = () => {
+  //  TODO: loader status: 
+  // const [isGettingLocation, setIsGettingLocation] = useState(false);
+  const [data, setData] = useLocalStorage('locationData', initialState)
+  const [state, dispatch] = useReducer(loggerReducer, data)
+
+
+  useEffect(() => {
+    setData(state)
+  }, [state, setData])
+
+  const { selectedLocation, coordinates } = state
+
+  const addCoordinates = coordinates => {
+    dispatch({
+      type: 'SET_SELECTED_COORDINATES',
+      payload: { coordinates }
+    })
+  }
+
+  const addCurrentLocation = () => {
+    if (navigator.geolocation) {
+      // TODO: loader status: setIsGettingLocation(true);
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lattitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          const coordinates = [longitude, lattitude];
+          // TODO: loader status: setIsGettingLocation(false);
+          addCoordinates(coordinates);
+        },
+        () =>
+          setIsGettingLocation(false)
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      // TODO: loader status: setIsGettingLocation(false);
+      console.log('the geolocation service is not supported in your browser');
+    }
+  };
+
+  const addGoogleLocation = (suggestion, coordinates) => {
+    dispatch({
+      type: 'SET_SELECTED_LOCATION',
+      payload: {suggestion, coordinates}
+    })
+  }
+
+  return { selectedLocation, coordinates, addCurrentLocation, addGoogleLocation }
+};
+
+export const [LocationProvider, useLocationContext] = constate(useCurrentLocation);
