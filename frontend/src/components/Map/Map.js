4f9e019f262065e3b2bf947dfa0baf3066ee5b00
Akolade <adesanmiakoladedotun@gmail.com>
2020-04-23 14:39:12 +0100

    remove unused context api

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { GoogleMap, OverlayView, Marker, useLoadScript, } from '@react-google-maps/api'
 import mapStyles from '../../shared/mapStyles.json'
 import banner from '../../assets/info_banner.png'
 import marker from '../../assets/marker.svg'
 import Button from '../Button/Button';
-import useHomeLocationQuery from '../../context/useHomeLocationQuery';
+import { useLocationContext } from '../../context/useCurrentLocation';
 
 
 const locations = [
@@ -21,12 +21,20 @@ const locations = [
 ]
 
 const Map = () => {
-  const [lng, lat] = useHomeLocationQuery();
-
   const [showInfo, setShowInfo] = useState(false)
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyCHTt_h9Drz0TcymU_qmYQWI2zvnsQkkQc"
   })
+
+  const { setCurrentLocation, coordinates: [lng, lat] } = useLocationContext()
+
+  const mountOnce = () => {
+    setCurrentLocation();
+    const unmount = () => console.log('unmounted');
+    return unmount
+  }
+
+  useEffect(mountOnce, []);
 
   if (loadError) {
     return <div>Map cannot be loaded right now, sorry.</div>
