b779ed4e29819f01a237acdb6cd4d883a9f2206a
Naomi Wickham <ns.wickham08@outlook.com>
2020-04-25 15:39:44 +0100

    Modal.js

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,41 +1,42 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import "./Modal.scss";
 import Button from "../Button/Button";
 
-function Share() {
-  const [modalShow, setModalShow] = useState(false);
+function Modal(props) {
+     const [modalShow, setModalShow] = useState(false);
 
-  const modalRef = useRef();
-  const handleClick = () => {
-    setModalShow(!modalShow);
-  };
-  window.addEventListener("click", function (e) {
-    if (e.target === modalRef.current) {
-      setModalShow(false);
-    }
-  });
+     const handleClick = () => {
+           setModalShow(false);
+           }
 
-    setTimeout(() => {
-      setModalShow(false);
-    }, 3000);
-  
+     window.onload=
+        setTimeout(() => {
+        setModalShow(true);
+        }, 3000)
 
-  return (
-    <div className="Modal">
-
+    return (
+<div>
       <div
         className={`Modal Modal--${modalShow ? "opened" : "closed"}`}
-        ref={modalRef}
+
       >
         <div className="Modal-body">
-        <Button type="dismiss" label="x" onClick={handleClick} />
-  
-         
-     
+                <div className="Modal-header">
+                    <h3>{props.children}</h3>
+                    <span className="Close-modal-btn" onClick={handleClick}>×</span>
+                </div>
+                <div className="Modal-content">
+                    <p>
+                       {props.children}
+                    </p>
+                </div>
+                <div className="Modal-footer">
+                    <Button className="btn-primary">Sign Up</Button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+     </div>
+    )
 }
 
 export default Modal;
