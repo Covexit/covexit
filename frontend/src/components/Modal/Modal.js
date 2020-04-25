3de5fdd966e25904941a736f31ef1a20141f929a
Naomi Wickham <ns.wickham08@outlook.com>
2020-04-25 21:53:16 +0100

    removed button

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,11 +1,10 @@
 import React, { useState, useRef } from "react";
 import "./Modal.scss";
-import Button from "../Button/Button";
 
 const Modal = ({ header, body, footer }) => {
   const [modalShow, setModalShow] = useState(true);
   const modalRef = useRef();
-  const handleClick = () => {
+  const onClose = () => {
     setModalShow(false);
   }
 
@@ -15,28 +14,21 @@ const Modal = ({ header, body, footer }) => {
     }
   });
 
-   header="Sign Up!"
-   body = "Recieve updates and stay informed!"
-   footer="*this is a disclaimer"
-
   return (
- <div>
+
     <div
       className={`Modal Modal--${modalShow ? "opened" : "closed"}`}
       ref={modalRef}
     >
       <div className="Modal-body">
         <div className="Modal-header">
-                <span className="Close-modal-btn" onClick={handleClick}>x</span>
-                {header}
+        <span className="Close-modal-btn" onClose={onClose}>x</span>
+        {header}
         </div>
         <div className="Modal-content">{body}</div>
-
-        <Button className="btn-primary">Sign Up</Button>
         <div className="Modal-footer">{footer}</div>
       </div>
-      </div>
-  </div>
+    </div>
   )
 }
 
