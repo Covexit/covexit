da7bc2b15685ce175b86808021f192559a4f125a
Naomi Wickham <ns.wickham08@outlook.com>
2020-04-25 16:02:49 +0100

    dismiss modal if clicked outside

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,21 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef} from "react";
 import "./Modal.scss";
 import Button from "../Button/Button";
 
 function Modal(props) {
-     const [modalShow, setModalShow] = useState(true);
+    const [modalShow, setModalShow] = useState(true);
+    const modalRef = useRef();
+    const handleClick = () => {
+          setModalShow(false);
+         }
 
-     const handleClick = () => {
-           setModalShow(false);
-           }
-
-
+    window.addEventListener("click", function (e) {
+      if (e.target !== modalRef.current) {
+        setModalShow(false);
+      }
+    });
 
     return (
-
+<div>
       <div
         className={`Modal Modal--${modalShow ? "opened" : "closed"}`}
-
+        ref={modalRef}
       >
         <div className="Modal-body">
                 <div className="Modal-header">
@@ -32,7 +36,7 @@ function Modal(props) {
                 </div>
             </div>
         </div>
-   
+     </div>
     )
 }
 
