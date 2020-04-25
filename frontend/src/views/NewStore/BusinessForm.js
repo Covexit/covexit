8adfd8cf762e10b0fe08c587651446741a86a0aa
David Doan <44115294+Davoshy@users.noreply.github.com>
2020-04-25 15:28:08 +0200

    Update frontend/src/views/NewStore/BusinessForm.js
    Co-Authored-By: Philipp <VellerPhilipp@gmail.com>

--- frontend/src/views/NewStore/BusinessForm.js
+++ frontend/src/views/NewStore/BusinessForm.js
@@ -62,8 +62,8 @@ const BusinessForm = ({ location, history }) => {
     let searchString = data.line1 + " , " + data.line2;
     const getLocation = await axios(
         `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${searchString}&format=json`)
-    lat = Number(getLocation.data[0].lat).toFixed(5);
-    lon = Number(getLocation.data[0].lon).toFixed(5)
+    const latitude = Number(getLocation.data[0].lat).toFixed(5);
+    const longitude = Number(getLocation.data[0].lon).toFixed(5)
 
     const response = await API.partners.post({
       ...data, address: { ...data, latitude: lat, longitude: lon }, users: [user.id]
