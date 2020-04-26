5daea2d3e2a9a19a7aa3d378084443255a67edee
Philipp Veller <vellerphilipp@gmail.com>
2020-04-26 16:33:46 +0200

    lint fix

--- frontend/src/components/PhotoSelector/PhotoSelector.js
+++ frontend/src/components/PhotoSelector/PhotoSelector.js
@@ -2,38 +2,33 @@ import React from 'react'
 import checkmark from '../../assets/checkmark.svg'
 
 import './PhotoSelector.scss'
-import { useTranslation } from 'react-i18next';
-import Button from '../Button/Button';
 
-const PhotoSelector = ({ images, onSelected, photo }) => {
-  const [t] = useTranslation('owner-photo-select');
+const PhotoSelector = ({ images, onSelected, photo }) => (
+  <div className="PhotoSelector">
+    {images.map((image, i) =>
+      <label
+        key={i}
+        className={`PhotoSelector-option
+          ${photo === image && 'PhotoSelector-option--selected'}
+        `}
+      >
+        <input
+          className="PhotoSelector-radio"
+          type="radio"
+          value={i}
+          checked={photo === image}
+          onChange={() => onSelected(image)}
+        />
 
-  return (
-    <div className="PhotoSelector">
-      {images.map((image, i) =>
-        <label
-          key={i}
-          className={`PhotoSelector-option
-            ${photo === image && 'PhotoSelector-option--selected'}
-          `}
-        >
-          <input
-            className="PhotoSelector-radio"
-            type="radio"
-            value={i}
-            checked={photo === image}
-            onChange={() => onSelected(image)}
-          />
+        <img className="PhotoSelector-photo" src={`/photos/${image}`} alt={i} />
 
-          <img className="PhotoSelector-photo" src={`/photos/${image}`} alt={i} />
+        {photo === image &&
+          <img className="PhotoSelector-checkmark" src={checkmark} alt="checkmark" />
+        }
+      </label>
+    )}
+  </div>
+);
 
-          {photo === image &&
-            <img className="PhotoSelector-checkmark" src={checkmark} alt="checkmark" />
-          }
-        </label>
-      )}
-    </div>
-  )
-}
 
 export default PhotoSelector
