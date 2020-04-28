3282c0759cc5e8240e3e7c87398c4477cf6bc791
Naomi Wickham <ns.wickham08@outlook.com>
2020-04-28 15:36:06 +0100

    Loader Message

--- frontend/src/components/Loader/Loader.js
+++ frontend/src/components/Loader/Loader.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './Loader.scss';
 
-function Loader() {
-  return (
-    <div className="Loader">
+function Loader({children}) {
 
+  const currentTime = new Date();
+  const hour = currentTime.getHours();
+  const minute = currentTime.getMinutes();
+  const minutesDegrees =  (minute/60) * 360 + 90;
+  const hourDegrees = (hour/12) * 360 + 90;
+  const hourStyle = {hourDegrees};
+  const minuteStyle = {minutesDegrees};
+
+  return (
+    <div className="Loader-body">
+      <div className="Message">{children}</div>
+        <div className="Loader">
+          <div className="hand hour" style={hourStyle} />
+          <div className="hand minute" style={minuteStyle} />
+        </div>
     </div>
 
   );
 }
 
 export default Loader;
+
