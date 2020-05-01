0294d13d6f635f16e763564f197ebe5dea4de97e
Philipp Veller <vellerphilipp@gmail.com>
2020-05-01 23:54:37 +0200

    adjust footer style

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,14 +2,29 @@ import React from "react";
 import "./Footer.scss";
 import logo from "assets/logo.svg";
 import { Link } from "react-router-dom";
-import { FaRegCopyright } from "react-icons/fa";
+import { useTranslation } from 'react-i18next';
 
 function Footer() {
+  const [t] = useTranslation('menu');
+  const links = [
+    {route: '/imprint', label: t('imprint')},
+    {route: '/tos', label: t('tos')},
+    {route: '/privacy', label: t('privacy')},
+  ];
+
   return (
     <div className="Footer">
-      <Link to="/" className="Footer-logo"><img src={logo} alt="Covexit Logo" /></Link>
-      <div className="Footer-copyright"><FaRegCopyright /> 2020 Covexit</div>
-      <Link to="/" className="Footer-link"></Link>
+      <div className="Footer-body">
+        <Link to="/" className="Footer-logo"><img src={logo} alt="Covexit Logo"/></Link>
+        <div className="Footer-copyright">&copy; 2020 Covexit</div>
+        <nav>
+          <ul className="Footer-links">
+            {
+              links.map(link => <li className="Footer-link"><Link to={link.route}>{link.label}</Link></li>)
+            }
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
