ca6b074c113c41a5bb9f57471416bc807905575c
Akolade <adesanmiakoladedotun@gmail.com>
2020-05-04 14:20:46 +0100

    improve confirmation screen

--- frontend/src/components/Yippey/Yippey.js
+++ frontend/src/components/Yippey/Yippey.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import gift from '../../assets/gift.svg';
 
-const Yippey = ({ text }) => (
-  <>
+import './Yippey.scss'
+
+const Yippey = ({ text, container }) => (
+  <div className={`Yippey Yippey--${container ? 'container': ''}`}>
     <img src={gift} alt="Gift"/>
     <h1>Yippey!</h1>
     <p>{text}</p>
-  </>
+  </div>
 )
 
 export default Yippey
