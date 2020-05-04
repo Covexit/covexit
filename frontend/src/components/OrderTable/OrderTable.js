f31f3436614df9ddff02e88ede77e68d13638833
Akolade <adesanmiakoladedotun@gmail.com>
2020-05-04 14:49:17 +0100

    fix order table indentation

--- frontend/src/components/OrderTable/OrderTable.js
+++ frontend/src/components/OrderTable/OrderTable.js
@@ -12,43 +12,43 @@ function OrderTable(props) {
 
   return (
     <Fragment>
-    <table className="OrderTable">
-      <tbody>
-      <tr>
-        <th>{t('nameRow')}:</th>
-        <td><h2>{props.name}</h2></td>
-      </tr>
-      <tr>
-        <th>{t('productsRow')}:</th>
-        <td>
-          {props.products.map(e =>
-            <div className="OrderTable-product" key={e.name}>
-              {e.quantity} x {e.name}
-              <span className="OrderTable-price">{e.price.toFixed(2)}€</span>
-            </div>)}
-        </td>
-      </tr>
-      <tr>
-        <th>{t('addressRow')}:</th>
-        <td>
-          {props.street}<br/>{props.zipcity}
-          {matchPath && <>
-            <br />
-            <div className="view-on-maps">
-              <Button secondary label="View on Maps" />
-            </div>
-            </>}
-        </td>
-      </tr>
-      <tr>
-        <th><h2>{t('totalRow')}</h2></th>
-        <td className="OrderTable-total">
-          <h2>{props.products.reduce((prev, current) => prev.price + current.price).toFixed(2)}€</h2>
-          <p>{t('VAT')}</p>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+      <table className="OrderTable">
+        <tbody>
+        <tr>
+          <th>{t('nameRow')}:</th>
+          <td><h2>{props.name}</h2></td>
+        </tr>
+        <tr>
+          <th>{t('productsRow')}:</th>
+          <td>
+            {props.products.map(e =>
+              <div className="OrderTable-product" key={e.name}>
+                {e.quantity} x {e.name}
+                <span className="OrderTable-price">{e.price.toFixed(2)}€</span>
+              </div>)}
+          </td>
+        </tr>
+        <tr>
+          <th>{t('addressRow')}:</th>
+          <td>
+            {props.street}<br/>{props.zipcity}
+            {matchPath && <>
+              <br />
+              <div className="view-on-maps">
+                <Button secondary label="View on Maps" />
+              </div>
+              </>}
+          </td>
+        </tr>
+        <tr>
+          <th><h2>{t('totalRow')}</h2></th>
+          <td className="OrderTable-total">
+            <h2>{props.products.reduce((prev, current) => prev.price + current.price).toFixed(2)}€</h2>
+            <p>{t('VAT')}</p>
+          </td>
+        </tr>
+        </tbody>
+      </table>
       {matchPath &&<div className="OrderTable-confirm">
         <Button to="/orders/confirm" type="confirm" label="Mark accept" />
       </div>
