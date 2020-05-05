9cd35ed3d0d2187d111a623265fda5367cf5dd4c
Philipp Veller <vellerphilipp@gmail.com>
2020-05-06 00:55:26 +0200

    filter products by class

--- backend/covexit/api_extensions/views/admin/partner.py
+++ backend/covexit/api_extensions/views/admin/partner.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from oscarapi.views.admin.partner import PartnerList as _PartnerList
 from oscarapi.views.admin.partner import PartnerDetail as _PartnerDetail
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -7,6 +8,13 @@ from covexit.api_extensions.permissions import BelongsTo
 
 class PartnerList(_PartnerList):
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user and self.request.user.is_active:
+            return qs.filter(Q(is_active=True) | Q(is_active=False,
+                                                   users__exact=self.request.user.pk))
+        return qs.filter(is_active=True)
 
 
 class PartnerDetail(_PartnerDetail):
