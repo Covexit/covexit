274285e2922073b607f2cded49667aba0d93eb92
Philipp Veller <vellerphilipp@gmail.com>
2020-05-06 01:08:23 +0200

    memoize api object to prevent any loops

--- frontend/src/shared/api.js
+++ frontend/src/shared/api.js
@@ -1,45 +1,46 @@
 import axios from 'axios';
 import { useUserContext } from '../context/UserContext';
+import React from 'react';
+
+
+const axiosInstance = axios.create({
+  baseURL: '/api/v1/',
+  responseType: 'json',
+});
+
+function endpointTransform(endpoint, id) {
+  let needle = '$id';
+  if (!id)
+    needle = '$id/';
+  return endpoint.replace(needle, id || '');
+}
+
+function createEndpoint(endpoint) {
+  return {
+    get: (id) => axiosInstance.get(endpointTransform(endpoint, id)),
+    put: (id, data, config) => axiosInstance.put(endpointTransform(endpoint, id), data, config),
+    patch: (id, data, config) => axiosInstance.patch(endpointTransform(endpoint, id), data, config),
+    post: (data, config, id) => axiosInstance.post(endpointTransform(endpoint, id), data, config),
+  }
+}
+
+function createHyperlinkedEndpoint(endpoint) {
+  return {
+    get: ({ id, url }) => axios.get(url || `/api/v1/${endpointTransform(endpoint, id)}`),
+    patch: ({ id, url, data, config }) => axios.patch(url || `/api/v1/${endpointTransform(endpoint, id)}`, data, config),
+    post: ({ data, config, id }) => axios.post(`/api/v1/${endpointTransform(endpoint, id)}`, data, config),
+  }
+}
 
 const useApi = () => {
   const { token } = useUserContext();
-  const axiosInstance = axios.create({
-    baseURL: '/api/v1/',
-    responseType: 'json',
-
-  });
-
   axiosInstance.interceptors.request.use(config => {
     if (token)
-      config = {...config, headers: { 'Authorization': `Token ${token}` }};
+      config = { ...config, headers: { 'Authorization': `Token ${token}` } };
     return config;
   })
 
-  function endpointTransform(endpoint, id) {
-    let needle = '$id';
-    if (!id)
-      needle = '$id/';
-    return endpoint.replace(needle, id || '');
-  }
-
-  function createEndpoint(endpoint) {
-    return {
-      get: (id) => axiosInstance.get(endpointTransform(endpoint, id)),
-      put: (id, data, config) => axiosInstance.put(endpointTransform(endpoint, id), data, config),
-      patch: (id, data, config) => axiosInstance.patch(endpointTransform(endpoint, id), data, config),
-      post: (data, config, id) => axiosInstance.post(endpointTransform(endpoint, id), data, config),
-    }
-  }
-
-  function createHyperlinkedEndpoint(endpoint) {
-    return {
-      get: ({ id, url }) => axios.get(url || `/api/v1/${endpointTransform(endpoint, id)}`),
-      patch: ({ id, url, data, config }) => axios.patch(url || `/api/v1/${endpointTransform(endpoint, id)}`, data, config),
-      post: ({ data, config, id }) => axios.post(`/api/v1/${endpointTransform(endpoint, id)}`, data, config),
-    }
-  }
-
-  const API = {
+  const API = React.useMemo(() => ({
     categories: createHyperlinkedEndpoint('categories/'),
     register: { post: (data) => axiosInstance.post('register/', data) },
     verify: { post: (data, type) => axiosInstance.post(`verify/${type}/`, data) },
@@ -49,7 +50,7 @@ const useApi = () => {
     products: createHyperlinkedEndpoint('admin/products/$id/'),
     productImages: createEndpoint('admin/products/$id/images/'),
     authToken: { post: (data) => axiosInstance.post(`api-token-auth/`, data) },
-  };
+  }), []);
 
   return { API, axiosInstance };
 }
