ff0e474f2bcc01c9f4280a133cad776e5566ad64
Philipp Veller <vellerphilipp@gmail.com>
2020-05-07 01:19:48 +0200

    add logged in state to header

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -3,25 +3,29 @@ import './Menu.scss';
 import { NavLink } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
 import Button from '../Button/Button';
-import { useUserContext } from '../../context/UserContext';
+import { useUserContext } from 'context/UserContext';
 
 
-function Menu() {
+function Menu({ partner }) {
+  let links;
   const [menuOpen, setMenuOpen] = useState(false);
   const [t] = useTranslation(['menu', 'account']);
-  const { isAuthenticated, logoutSuccess} = useUserContext();
+  const { isAuthenticated, logoutSuccess } = useUserContext();
 
-  const links = [
-    { to: 'https://covexit.webflow.io/', label: t('menu:howItWorks'), external: true },
-    { to: '/stores', label: t('menu:explore') },
-  ];
-
-  const logoutHandler = () => {
-    logoutSuccess();
-  };
+  if (partner.name)
+    links = [
+      <NavLink to={`/stores/${partner.id}/`}>{t('menu:products')}</NavLink>,
+      <NavLink to={`/stores/${partner.id}/orders`}>{t('menu:orders')}</NavLink>,
+    ];
+  else
+    links = [
+      <a href="https://covexit.webflow.io/">{t('menu:howItWorks')}</a>,
+      <NavLink to={`/stores`}>{t('menu:explore')}</NavLink>,
+      <Button to="/stores/new" label={t('menu:merchantSignUp')} type="small"/>
+    ];
 
   const loginField = <Button to="/login" label={t('account:login')} type="small"/>;
-  const logoutField = <Button onClick={logoutHandler} to="/" label={t('account:logout')} type="small"/>;
+  const logoutField = <Button onClick={() => logoutSuccess()} to="/" label={t('account:logout')} type="small"/>;
 
   return (
     <nav className={`Menu Menu--${menuOpen ? 'opened' : 'closed'}`}>
@@ -31,17 +35,10 @@ function Menu() {
       </button>
       <div className="Menu-body">
         <ul className="Menu-list">
-          {links.map(e =>
-            <li className="Menu-list-item" key={e.label}>
-              {(e.external && <a href={e.to} className="Menu-link">{e.label}</a>) ||
-              <NavLink to={e.to} onClick={() => setMenuOpen(false)} className={`Menu-link ${e.meta && 'Menu-link--meta'}`}>{e.label}</NavLink>}
-            </li>)}
-            <li className="Menu-list-item Menu-link">
-              <Button to="/stores/new" label={t('menu:merchantSignUp')} type="small"/>
-            </li>
-            <li className="Menu-list-item Menu-link">
-              {isAuthenticated ? logoutField : loginField}
-            </li>
+          {links.map(component => <li className="Menu-list-item Menu-link" key={component}>{component}</li>)}
+          <li className="Menu-list-item Menu-link">
+            {isAuthenticated ? logoutField : loginField}
+          </li>
         </ul>
         <div className="Menu-footer">© 2020 Covexit</div>
       </div>
