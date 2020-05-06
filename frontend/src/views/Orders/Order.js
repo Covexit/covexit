0380bab32f50d23548741f8c921deca58ba93c42
Akolade <adesanmiakoladedotun@gmail.com>
2020-05-06 02:40:00 +0100

    cleanup nested destructuring

--- frontend/src/views/Orders/Order.js
+++ frontend/src/views/Orders/Order.js
@@ -10,14 +10,14 @@ const activeOrderStyle = 'Order-category-item--active';
 
 const Order = (props) => {
   const [t] = useTranslation('order');
-  const { location: { pathname, }, match: { params: { id } } } = props;
-  const path = matchPath(pathname, { path: '/stores/:id/orders/:orderId' });
+  const { location, match } = props;
+  const pathMatch = matchPath(location.pathname, { path: '/stores/:id/orders/:orderId' });
 
   return (
     <div className="Order">
       <div className="Order-category">
-        <Link to={`/stores/${id}/orders`} className={`Order-category-item ${!path || !path.params.orderId ? activeOrderStyle : ''}`}>{t('newOrders')}</Link>
-        <Link to={`/stores/${id}/orders/history`} className={`Order-category-item ${path && path.params.orderId ? activeOrderStyle : ''}`}>{t('orderHistory')}</Link>
+        <Link to={`/stores/${match.params.id}/orders`} className={`Order-category-item ${!pathMatch || !pathMatch.params.orderId ? activeOrderStyle : ''}`}>{t('newOrders')}</Link>
+        <Link to={`/stores/${match.params.id}/orders/history`} className={`Order-category-item ${pathMatch && pathMatch.params.orderId ? activeOrderStyle : ''}`}>{t('orderHistory')}</Link>
       </div>
       <div className="OrderItems">
         <OrderItem {...props} />
