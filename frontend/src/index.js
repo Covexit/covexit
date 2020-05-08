ac51c09e271a40e04458410b69485e53e974d954
Philipp Veller <vellerphilipp@gmail.com>
2020-05-08 22:42:39 +0200

    remove osano, aint working, try cookiebot

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,6 @@ import App from './app/App';
 import * as serviceWorker from 'serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 import Loader from './components/Loader/Loader';
-import Script from 'react-load-script';
-
 import './i18n';
 
 ReactDOM.render(
@@ -14,7 +12,6 @@ ReactDOM.render(
     <BrowserRouter>
       <Suspense fallback={<Loader />}>
         <App/>
-        <Script url="https://cmp.osano.com/16BZCGRyPlDXKACB/66a05808-c94b-4134-998a-d1be0f6e4d64/osano.js"/>
       </Suspense>
     </BrowserRouter>
   </React.StrictMode>,
