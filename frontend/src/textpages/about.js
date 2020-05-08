ceea1dc3b86ac9f4784363d771b0764333ea9c75
Naomi Wickham <ns.wickham08@outlook.com>
2020-05-08 18:00:26 +0100

    move images to assets and update js

--- frontend/src/textpages/about.js
+++ frontend/src/textpages/about.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import "./About.scss";
+import naomi from '../assets/team/naomi.png';
+import fabian_katz from '../assets/team/fabian_katz.png';
+import moritz from '../assets/team/moritz.png';
+import marco from '../assets/team/marco.png';
+import nicolai from '../assets/team/nicolai.png';
+import fabian from '../assets/team/fabian.png';
+import lea from '../assets/team/lea.png';
+import silvia from '../assets/team/silvia.png';
+import lisa from '../assets/team/lisa.png';
+import laura from '../assets/team/laura.png';
+import volker from '../assets/team/volker.png';
+import nico from '../assets/team/nico.png';
+import balint from '../assets/team/balint.png';
+import ines from '../assets/team/ines.png';
+import philipp from '../assets/team/philipp.png';
+import david from '../assets/team/david.png';
+import akolade from '../assets/team/akolade.png';
+import cynthia from '../assets/team/cynthia.png';
 
 
 const teamMembers = [
-    {id: '1', name: 'Fabian Katz', image: "./team/fabian_katz.png ", link: 'https://www.linkedin.com/in/fabian-katz/', position: 'Innovation'},
-    {id: '2', name: 'Moritz Giebel', image: "./team/moritz.png", link: 'https://www.linkedin.com/in/moritz-giebel-030054153/', position: 'Innovation'},
-    {id: '3', name: 'Marco Schmid', image: "./team/marco.png", link: 'https://www.linkedin.com/in/marco-schmid-1a39861a4/', position: 'Project co-ordination'},
-    {id: '4', name: 'Nicolai Huss', image: "./team/nicolai.png", link: 'https://www.linkedin.com/in/nicolai-huss/', position: 'Project co-ordination'},
-    {id: '5', name: 'Fabian Ochs', image: "./team/fabian.png", link: 'https://www.linkedin.com/in/fabian-ochs-395b701a7/', position: 'Marketing'},
-    {id: '6', name: 'Lea Joos', image: "./team/lea.png", link: 'https://www.linkedin.com/in/lea-joos-5808a5174/', position: 'Marketing'},
-    {id: '7', name: 'Silvia Mogas', image: "./team/silvia.png", link: 'https://www.linkedin.com/in/silviamogas/', position: 'Marketing'},
-    {id: '8', name: 'Lisa Heite', image: "./team/lisa.png", link: 'https://linkedin.com/in/lisa-heite', position: 'Marketing'},
-    {id: '9', name: 'Laura Maria Kassovicova', image: "./team/laura.png", link: 'https://www.linkedin.com/in/lauramariak/', position: 'Marketing'},
-    {id: '10', name: 'Volker Jetter', image: "./team/volker.png", link: 'https://www.linkedin.com/in/volker-jetter-0bb412ba/', position: 'Funding'},
-    {id: '11', name: 'Nico Buchsenstein', image: "./team/nico.png", link: 'https://www.linkedin.com/in/nicobuechsenstein/', position: 'Funding'},
-    {id: '12', name: 'Balint Csizmadia', image: "./team/balint.png", link: 'https://www.linkedin.com/in/balint-design/', position: 'Branding/UX design'},
-    {id: '13', name: 'Ines Filipp', image: "./team/ines.png", link: 'https://www.linkedin.com/company/wearecovexit/', position: 'Branding/UX design'},
-    {id: '14', name: 'Philipp Veller', image: "./team/philipp.png", link: 'https://www.linkedin.com/in/philipp-veller-webdev/', position: 'Lead Developer'},
-    {id: '15', name: 'David Doan', image: "./team/david.png", link: 'https://www.linkedin.com/in/ddoan89/', position: 'Developer'},
-    {id: '16', name: 'Akolade Adesanmi', image: "./team/akolade.png", link: 'https://www.linkedin.com/in/akolade-adesanmi-156276142/', position: 'Developer'},
-    {id: '17', name: 'Cynthia Mulenga', image: "./team/cynthia.png", link: 'https://www.linkedin.com/in/cynthiammulenga/', position: 'Developer'},
-    {id: '18', name: 'Naomi Wickham', image: "./team/naomi.png", link: 'https://www.linkedin.com/in/naomiwickham/', position: 'Developer'}, 
+    {id: '1', name: 'Fabian Katz', image: fabian_katz, link: 'https://www.linkedin.com/in/fabian-katz/', position: 'Innovation'},
+    {id: '2', name: 'Moritz Giebel', image: moritz, link: 'https://www.linkedin.com/in/moritz-giebel-030054153/', position: 'Innovation'},
+    {id: '3', name: 'Marco Schmid', image: marco, link: 'https://www.linkedin.com/in/marco-schmid-1a39861a4/', position: 'Project co-ordination'},
+    {id: '4', name: 'Nicolai Huss', image: nicolai, link: 'https://www.linkedin.com/in/nicolai-huss/', position: 'Project co-ordination'},
+    {id: '5', name: 'Fabian Ochs', image: fabian, link: 'https://www.linkedin.com/in/fabian-ochs-395b701a7/', position: 'Marketing'},
+    {id: '6', name: 'Lea Joos', image: lea, link: 'https://www.linkedin.com/in/lea-joos-5808a5174/', position: 'Marketing'},
+    {id: '7', name: 'Silvia Mogas', image: silvia, link: 'https://www.linkedin.com/in/silviamogas/', position: 'Marketing'},
+    {id: '8', name: 'Lisa Heite', image: lisa, link: 'https://linkedin.com/in/lisa-heite', position: 'Marketing'},
+    {id: '9', name: 'Laura Maria Kassovicova', image: laura, link: 'https://www.linkedin.com/in/lauramariak/', position: 'Marketing'},
+    {id: '10', name: 'Volker Jetter', image: volker, link: 'https://www.linkedin.com/in/volker-jetter-0bb412ba/', position: 'Funding'},
+    {id: '11', name: 'Nico Buchsenstein', image: nico, link: 'https://www.linkedin.com/in/nicobuechsenstein/', position: 'Funding'},
+    {id: '12', name: 'Balint Csizmadia', image: balint, link: 'https://www.linkedin.com/in/balint-design/', position: 'Branding/UX design'},
+    {id: '13', name: 'Ines Filipp', image: ines, link: 'https://www.linkedin.com/company/wearecovexit/', position: 'Branding/UX design'},
+    {id: '14', name: 'Philipp Veller', image: philipp, link: 'https://www.linkedin.com/in/philipp-veller-webdev/', position: 'Lead Developer'},
+    {id: '15', name: 'David Doan', image: david, link: 'https://www.linkedin.com/in/ddoan89/', position: 'Developer'},
+    {id: '16', name: 'Akolade Adesanmi', image: akolade, link: 'https://www.linkedin.com/in/akolade-adesanmi-156276142/', position: 'Developer'},
+    {id: '17', name: 'Cynthia Mulenga', image: cynthia, link: 'https://www.linkedin.com/in/cynthiammulenga/', position: 'Developer'},
+    {id: '18', name: 'Naomi Wickham', image: naomi, link: 'https://www.linkedin.com/in/naomiwickham/', position: 'Developer'}, 
 ]
 
 
