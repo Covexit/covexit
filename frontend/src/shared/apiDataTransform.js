1cf0ae2a3294024112c3ba9af3d90906498eec13
Philipp Veller <vellerphilipp@gmail.com>
2020-05-20 00:23:33 +0200

    flatten error response object and add handling to BusinessForm.js

--- frontend/src/shared/apiDataTransform.js
+++ frontend/src/shared/apiDataTransform.js
@@ -1,8 +1,24 @@
 export const apiErrorTransform = (data, errorResponse) => {
   const newState = { ...data };
-  for (const [key, value] of Object.entries(errorResponse)) {
+  // recursively flatten object
+  console.log(errorResponse);
+  const errorsFlat = Object.assign(
+    {},
+    ...function _flatten(o) {
+      return [].concat(...Object.keys(o)
+        .map(k =>
+          typeof o[k] === 'object' && !Array.isArray(o[k])?
+            _flatten(o[k]) :
+            ({ [k]: o[k].join(' ') }),
+        ),
+      );
+    }(errorResponse),
+  )
+  console.log(errorsFlat);
+  for (const [key, value] of Object.entries(errorsFlat)) {
+    console.log(data, key)
     if (!data[key].error) {
-      newState[key] = { value: data[key], error: value.join(' ') }
+      newState[key] = { value: data[key], error: value }
     }
   }
   return newState;
